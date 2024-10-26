33f7921aaec6cf25a372cf7d3a4509e920f099f3
joaolucas14 <jlparceiro10@gmail.com>
2024-10-26 15:12:45 -0300

    finalizando o menu lateral

--- src/componentes/BarraLaterel/index.jsx
+++ src/componentes/BarraLaterel/index.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import ItemNavegacao from "./ItemNavegacao"
 
 const ListaEstilizada = styled.ul`
     list-style: none;
@@ -12,12 +13,16 @@ const BarraLateral = ()=>{
         <aside>
             <nav>
                 <ListaEstilizada>
-                    <li>
-                        <a href="#">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="#">Mais Vistas</a>
-                    </li>
+                    <ItemNavegacao
+                        iconeAtivo="/icones/home-ativo.png"
+                        iconeInativo="/icones/home-inativo.png"
+                        ativo={true}
+                    >Inicio</ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo="/icones/mais-vistas-ativo.png"
+                        iconeInativo="/icones/mais-vistas-inativo.png"
+                    >Mais Vistas</ItemNavegacao>
+                    
                 </ListaEstilizada>
             </nav>
         </aside>
