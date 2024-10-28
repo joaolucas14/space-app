910bb42ffdf36232035ff25d89f0905dd1ac8cda
joaolucas14 <jlparceiro10@gmail.com>
2024-10-28 12:48:47 -0300

    iniciando a funcionalidade de zoom na imagem

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import bannerBackground from "./assets/banner.png"
 import Galeria from "./componentes/Galeria"
 import fotos from "./fotos.json"
 import { useState } from "react"
+import ModalZoom from "./componentes/ModalZoom"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -45,6 +46,7 @@ const App= () => {
             </ConteudoGaleria>
           </MainContainer>
         </AppContainer>
+        <ModalZoom/>
       </FundoGradiente>
   )
 }
