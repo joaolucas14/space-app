9781b7a95a25d164a31be0b879f5106863b3eafa
joaolucas14 <jlparceiro10@gmail.com>
2024-10-28 11:03:01 -0300

    colocando o banenr adicionando elementos a barra lateral e ajustando os containers

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,31 @@ import styled from "styled-components"
 import EstiloGlobais from "./componentes/EstilosGlobais"
 import Cabecalho from "./componentes/Cabecalho"
 import BarraLateral from "./componentes/BarraLaterel"
+import Banner from "./componentes/Banner"
+import bannerBackground from "./assets/banner.png"
+import Galeria from "./componentes/Galeria"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width:100%;
   min-height:100vh;`
+
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0px auto;
+  max-width: 100%;
+`
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`
+
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
   
 function App() {
 
@@ -14,8 +34,16 @@ function App() {
     <>
       <FundoGradiente>
         <EstiloGlobais/>
-        <Cabecalho/>
-        <BarraLateral/>
+        <AppContainer>
+          <Cabecalho/>
+          <MainContainer>
+            <BarraLateral/>
+            <ConteudoGaleria>
+              <Banner backgroundImage={bannerBackground} texto="A galeria mais completa de fotos do espaço!"/>
+              <Galeria/>
+            </ConteudoGaleria>
+          </MainContainer>
+        </AppContainer>
       </FundoGradiente>
     </>
   )
